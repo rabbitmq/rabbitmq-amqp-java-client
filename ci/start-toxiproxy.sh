738d5176c36c7c36df5100f239a2a8eeb60c9400
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2024-04-17 17:43:19 +0200

    Fix toxiproxy starting script

--- ci/start-toxiproxy.sh
+++ ci/start-toxiproxy.sh
@@ -4,8 +4,3 @@ docker rm -f toxiproxy 2>/dev/null || echo "toxiproxy was not running"
 docker run -d --name toxiproxy \
     --network host \
     ghcr.io/shopify/toxiproxy
-
-wait_for_message rabbitmq "completed with"
-
-docker exec rabbitmq rabbitmq-diagnostics erlang_version
-docker exec rabbitmq rabbitmqctl version
