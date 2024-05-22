4f934e28418573df75b1914f72ce83751464c750
rabbitmq-ci <rabbitmq-ci@users.noreply.github.com>
2024-05-22 13:44:42 +0000

    Add the metrics collector abstraction

--- 1.0-SNAPSHOT/api/package-search-index.js
+++ 1.0-SNAPSHOT/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.rabbitmq.model"},{"l":"com.rabbitmq.model.amqp"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.rabbitmq.model"},{"l":"com.rabbitmq.model.amqp"},{"l":"com.rabbitmq.model.metrics"}];updateSearchResults();
