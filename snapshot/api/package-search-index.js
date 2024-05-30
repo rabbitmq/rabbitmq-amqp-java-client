8f341e9896552675dacb3ea26ed127dd9ab57090
rabbitmq-ci <rabbitmq-ci@users.noreply.github.com>
2024-05-30 15:08:08 +0000

    Add support for distributed tracing
    With Micrometer Observation and Tracing.

--- snapshot/api/package-search-index.js
+++ snapshot/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.rabbitmq.model"},{"l":"com.rabbitmq.model.amqp"},{"l":"com.rabbitmq.model.metrics"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.rabbitmq.model"},{"l":"com.rabbitmq.model.amqp"},{"l":"com.rabbitmq.model.metrics"},{"l":"com.rabbitmq.model.observation.micrometer"}];updateSearchResults();
