196033a16619483b73c039b19b928cbda5942154
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2024-10-29 08:21:27 +0100

    Use --opt-in flag to enable khepri

--- ci/start-cluster.sh
+++ ci/start-cluster.sh
@@ -17,9 +17,9 @@ wait_for_message rabbitmq0 "completed with"
 
 docker exec rabbitmq0 rabbitmqctl await_online_nodes 3
 
-docker exec rabbitmq0 rabbitmqctl enable_feature_flag --experimental khepri_db
-docker exec rabbitmq1 rabbitmqctl enable_feature_flag --experimental khepri_db
-docker exec rabbitmq2 rabbitmqctl enable_feature_flag --experimental khepri_db
+docker exec rabbitmq0 rabbitmqctl enable_feature_flag --opt-in khepri_db
+docker exec rabbitmq1 rabbitmqctl enable_feature_flag --opt-in khepri_db
+docker exec rabbitmq2 rabbitmqctl enable_feature_flag --opt-in khepri_db
 
 docker exec rabbitmq0 rabbitmqctl cluster_status
 
